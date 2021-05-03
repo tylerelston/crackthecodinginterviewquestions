86dbac5b87349ca2e345980a9427c0aa22b2ef9f
Tyler Elston <24640479+tylerelston@users.noreply.github.com>
2021-05-03 11:28:23 -0600

    1.1

---
+++ Arrays and Strings/1.1isUnique.py
@@ -0,0 +1,25 @@
+# Implement an algorithm to determine if a string has all unique characters.
+# What if you cannot use additional data structures?
+
+def isUnique(string):
+    unique = set()
+    for s in string:
+        if s in unique:
+            return False
+        unique.add(s)
+    return True
+
+def isUniqueNoStructures(string):
+    if len(string) > 1:
+        string = sorted(string)
+        for i in range(len(string) - 1):
+            if string[i] == string[i+1]:
+                return False
+    return True
+
+
+assert(isUnique('abc') == True)
+assert(isUnique('abcc') == False)
+
+assert(isUniqueNoStructures('abc') == True)
+assert(isUniqueNoStructures('abcc') == False)
